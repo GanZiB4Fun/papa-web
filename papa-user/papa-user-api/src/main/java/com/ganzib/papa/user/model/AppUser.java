2578164cfce69fd2e49d6624ac78bf3b07c5a77d
GanZiB <ganzib4fun@gmail.com>
2018-06-29 15:37:04 +0800

    dasda

---
+++ papa-user/papa-user-api/src/main/java/com/ganzib/papa/user/model/AppUser.java
@@ -0,0 +1,18 @@
+package com.ganzib.papa.user.model;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: GanZiB
+ * Date: 2018-06-29
+ * Time: 下午2:08
+ * Email: ganzib4fun@gmail.com
+ */
+public class AppUser implements Serializable  {
+    private static final long serialVersionUID = -2269763107154538332L;
+    private String userId;
+    private String userName;
+
+}
